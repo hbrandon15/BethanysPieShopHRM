8e72098911f818b32d6d6fc132cc26a128b7ebe4
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-10 12:36:33 -0500

    Working with char and int
    Working with char and int

--- Program.cs
+++ Program.cs
@@ -11,7 +11,32 @@ namespace BethanysPieShopHRM
 
             bool isActive = true;
             double rating = 99.25;
-            // byte numberOfEmployees = 300;
+
+            double ratePerHour = 12.34;
+            int numberOfHoursWorked = 165;
+
+            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+
+            Console.WriteLine(currentMonthWage);
+
+            ratePerHour += 3; //ratePerHour = ratePerHour + 3;
+
+
+            if (currentMonthWage > 2000)
+            {
+                Console.WriteLine("Top Paid Employee");
+            }
+
+            int numberOfEmployees = 15;
+            numberOfEmployees--;
+
+            int intMaxValue = int.MaxValue;
+            int intMinValue = int.MinValue;
+
+            char userSelection = 'a';
+            char upperVersion = char.ToUpper(userSelection);
+            bool isDigit = char.IsDigit(userSelection);
+            bool isLetter = char.IsLetter(userSelection);
 
 
             Console.ReadLine();
