e1cc70c0b7cd1eb3bd086ed6d4d40892db400673
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-10 13:01:21 -0500

    Working with DateTime
    Working with DateTime

--- Program.cs
+++ Program.cs
@@ -39,6 +39,25 @@ namespace BethanysPieShopHRM
             bool isLetter = char.IsLetter(userSelection);
 
 
+            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
+            Console.WriteLine(hireDate);
+
+            DateTime exitDate = new DateTime(2021, 12, 11);
+
+            DateTime startDate = hireDate.AddDays(15);
+            Console.WriteLine(startDate);
+
+            DateTime currentDate = DateTime.Now;
+            bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+            DateTime startHour = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(8, 35, 0);
+            DateTime endHour = startHour.Add(workTime);
+
+
+            Console.WriteLine(startHour.ToLongDateString());
+            Console.WriteLine(endHour.ToShortTimeString());
+
             Console.ReadLine();
         }
     }
