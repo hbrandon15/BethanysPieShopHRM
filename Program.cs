b0b5ea6b09075890e8b98015f71046f0a99de335
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-10 14:47:59 -0500

    Working with strings
    Working with strings

--- Program.cs
+++ Program.cs
@@ -6,66 +6,25 @@ namespace BethanysPieShopHRM
     {
         static void Main(string[] args)
         {
-            var monthlyWage = 1234;
-            int months = 12, bonus = 1000;
-
-            var isActive = true;
-            var rating = 99.25;
-
-            double ratePerHour = 12.34;
-            int numberOfHoursWorked = 165;
-
-            long veryLongMonth = numberOfHoursWorked;
-            double d = 123456789.0;
-
-            int x = (int)d;
-
-            int intVeryLongMonth = (int)veryLongMonth;
-
-            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
-
-            Console.WriteLine(currentMonthWage);
-
-            ratePerHour += 3; //ratePerHour = ratePerHour + 3;
+            string firstName = "Bethany";
+            string lastName = "Smith";
 
 
-            if (currentMonthWage > 2000)
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification = string.Concat(firstName, lastName);
+
+
+            string empId = firstName.ToLower() + "-" + lastName.Trim().ToLower();
+
+            int length = empId.Length;
+
+            if (fullName.Contains("beth") || fullName.Contains("Beth"))
             {
-                Console.WriteLine("Top Paid Employee");
+                Console.WriteLine("It's Bethany!");
             }
 
-            int numberOfEmployees = 15;
-            numberOfEmployees--;
-
-            int intMaxValue = int.MaxValue;
-            int intMinValue = int.MinValue;
-
-            char userSelection = 'a';
-            char upperVersion = char.ToUpper(userSelection);
-            bool isDigit = char.IsDigit(userSelection);
-            bool isLetter = char.IsLetter(userSelection);
-
-
-            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
-            Console.WriteLine(hireDate);
-
-            DateTime exitDate = new DateTime(2021, 12, 11);
-
-            DateTime startDate = hireDate.AddDays(15);
-            Console.WriteLine(startDate);
-
-            DateTime currentDate = DateTime.Now;
-            bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-            DateTime startHour = DateTime.Now;
-            TimeSpan workTime = new TimeSpan(8, 35, 0);
-            DateTime endHour = startHour.Add(workTime);
-
-
-            Console.WriteLine(startHour.ToLongDateString());
-            Console.WriteLine(endHour.ToShortTimeString());
-
-
+            string subString = fullName.Substring(1, 3);
+            Console.WriteLine("Characters 2-4 of fullName are: " + subString);
 
 
             Console.ReadLine();
