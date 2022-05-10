2842cb2733bd2f67889eed49cc0faf2c1618298e
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-10 14:23:28 -0500

    Creating strings
    Creating strings

---
+++ TestingStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BethanysPieShopHRM
+{
+	public class TestingStrings
+	{
+		static void Main(string[] args)
+		{
+			string firstName = "Bethany";
+			string lastName = "Smith";
+
+			string fullName = "Bethany Smith";
+
+			string noValue = null;
+			string s; // the same as Null
+
+			s = firstName;
+
+			var userName = "BethanyS"; // implicit typing
+
+
+			Console.ReadLine();
+		}
+	}
+
+}
