145717c49f6c311545f596c51218f29b093ff70f
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-11 07:01:08 -0500

    Escaping Text and comparing strings
    Escaping Text and comparing strings

--- Program.cs
+++ Program.cs
@@ -26,7 +26,21 @@ namespace BethanysPieShopHRM
             string subString = fullName.Substring(1, 3);
             Console.WriteLine("Characters 2-4 of fullName are: " + subString);
 
+            string nameUsingInterpolation = $"{firstName}-{lastName}";
 
+            string greeting = $"Hello, {firstName.ToLower()}";
+            Console.WriteLine(greeting);
+
+            string displayName = $"Welcome!\n{firstName}\t{lastName}";
+            Console.WriteLine(displayName);
+
+
+            string name1 = "Bethany";
+            string name2 = "bethany";
+
+            Console.WriteLine("Are both names equal?" + (name1 == name2));
+            Console.WriteLine("Is name equal to Bethany?" + (name1 == "Bethany"));
+            Console.WriteLine("Is name equal to BETHANY?" + name2.Equals("BETHANY"));
             Console.ReadLine();
         }
     }
