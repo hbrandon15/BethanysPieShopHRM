9e66c7f20ac895cb93df214157a9cdd697ae139a
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-12 08:18:38 -0500

    Working with several objects
    Working with several objects

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@ namespace BethanysPieShopHRM
             Console.WriteLine("-------------------\n");
             Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), EmployeeType.Manager, 25);
 
+            Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), EmployeeType.Research, 25);
+
+            Employee testEmployee = bethany;
+            testEmployee.firstName = "Gill";
+            testEmployee.DisplayEmployeeDetails();
+            bethany.DisplayEmployeeDetails();
+
+            bethany.firstName = "John";
+            bethany.hourlyRate = 10;
+
             bethany.DisplayEmployeeDetails();
             bethany.PerformWork();
             bethany.PerformWork();
@@ -18,6 +28,12 @@ namespace BethanysPieShopHRM
             bethany.PerformWork();
             bethany.ReceiveWage();
 
+            george.DisplayEmployeeDetails();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.ReceiveWage();
 
             Console.ReadLine();
 
